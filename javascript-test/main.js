f311e51509d767bc04efa546165394f7cd0f5638
Maria Pyshna <lunarocket@mail.ru>
2017-12-07 11:29:11 +0100

    Javascript test

---
+++ javascript-test/main.js
@@ -0,0 +1,24 @@
+console.log("Starting Javascript");
+
+var name = "Maria";
+
+console.log(name);
+
+
+var myAge =27;
+console.log(myAge);
+
+var averageAge = 30;
+var ageDiff = myAge - averageAge;
+
+if (ageDiff > 0) {
+	console.log("I am older than the average")
+}
+
+else if (ageDiff < 0) {
+	console.log("I am younger than the average")
+}
+
+else {
+	console.log("I am as old as the average")
+};
